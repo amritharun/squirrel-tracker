3220ee2e0b73183e5130c7f56d049787f2fa4530
cg3215 <cg3215@columbia.edu>
2020-10-08 11:18:35 -0400

    Added views

---
+++ map/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+from sightings.models import Squirrel
+
+def map(request,*args,**kwargs):
+    sightings = Squirrel.objects.all()[:100]
+    context={
+        'sightings':sightings        
+    }     
+    return render(request,'map/map.html',context)
