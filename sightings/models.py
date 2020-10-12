eb212c70a86211d814a1870f15e4bb045f0f76e9
cg3215 <cg3215@columbia.edu>
2020-10-11 21:00:09 -0400

    Added x, y and imports

--- sightings/models.py
+++ sightings/models.py
@@ -2,5 +2,16 @@ from django.db import models
 from django.utils.translation import gettext as _
 
 #Create Squirrel model
+class Squirrel(models.Model):
 
+    X = models.FloatField(
+        help_text=_('X'),
+        null=True,
+    )
+    
+    Y = models.FloatField(
+        help_text=_('Y'),
+        null=True,
+    )
+    
 
