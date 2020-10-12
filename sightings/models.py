450f1fe64a24162e018f1aecc1adc0b2e8b2c56c
cg3215 <cg3215@columbia.edu>
2020-10-11 21:19:34 -0400

    Update format

--- sightings/models.py
+++ sightings/models.py
@@ -15,7 +15,8 @@ class Squirrel(models.Model):
         help_text=_('Y'),
         null=True,
     )
-Unique_Squirrel_ID = models.CharField(
+
+    Unique_Squirrel_ID = models.CharField(
         help_text=_('Unique Squirrel ID'),
         max_length=14,
         primary_key=True,
@@ -54,5 +55,184 @@ Unique_Squirrel_ID = models.CharField(
         max_length=10,
         null=True,
     )
+
+    ADULT = 'Adult'
+    JUVENILE = 'Juvenile'
+    UNKNOWN = 'Unknown'
+    BLANK = ''
+
+    Age = models.CharField(
+        help_text=_('Age'),
+        choices = (
+            (ADULT, 'Adult'),
+            (JUVENILE, 'Juvenile'),
+            (UNKNOWN, '?'),
+            (BLANK, ''),
+        ),
+        default = BLANK,
+        max_length=10,
+        null=True,
+        blank=True,
+    )
+
+    GRAY = 'Gray'
+    BLACK = 'Black'
+    CINNAMMON = 'Cinnammon'
+    BLANK = ''
     
+    Primary_Fur_Color = models.CharField(
+        help_text=_('Primary Fur Color'),
+        choices = (
+            (GRAY, 'Gray'),
+            (BLACK, 'Black'),
+            (CINNAMMON, 'Cinnammon'),
+            (BLANK, ''),
+        ),
+        default = BLANK,
+        max_length = 10,
+        null=True,
+        blank=True,
+    )
+
+    G = 'Gray'
+    B = 'Black'
+    C = 'Cinnammon'
+    W = 'White'
+    BC = 'Black, Cinnammon'
+    BW = 'Black, White'
+    BCW = 'Black, Cinnammon, White'
+    CW = 'Cinnammon, White'
+    GB = 'Gray, Black'
+    BW = 'Black, White'
+    GW = 'Gray, White'
+    BLANK = ''
+
+    Highlight_Fur_Color = models.CharField(
+        help_text=_('Highlight Fur Color'),
+        choices = (
+            (G, 'Gray'),
+            (B, 'Black'),
+            (C, 'Cinnammon'),
+            (W, 'White'),
+            (BC, 'Black, Cinnammon'),
+            (BW, 'Black, White'),
+            (BCW, 'Black, Cinnammon, White'),
+            (CW, 'Cinammon, White'),
+            (GB, 'Gray, Black'),
+            (BW, 'Black, White'),
+            (GW, 'Gray, White'),
+            (BLANK, ''),
+        ),
+        default = BLANK,
+        max_length = 30,
+        null=True,
+        blank=True,
+    )
+
+    Combination_Fur = models.CharField(
+        help_text=_('Combination of Primary and Highlight Color'),
+        max_length = 100,
+        null=True,
+        default='+'
+    )
+
+    Color_Notes = models.CharField(
+        help_text=_('Color notes'),
+        max_length=100,
+        null=True,
+        blank=True,
+    )
+    AG = 'Above Ground'
+    GP = 'Ground Plane'
+    BLANK = ''
+
+    Location = models.CharField(
+        help_text=_('Location'),
+        choices = (
+            (AG, 'Above Ground'),
+            (GP, 'Ground Plane'),
+            (BLANK, ''),
+        ),
+        default = BLANK,
+        max_length=20,
+        null=True,
+        blank=True,
+    )
+
+    TRUE = 'true'
+    FALSE = 'false'
+
+    Above_Ground_Sighter_Measurement = models.CharField(
+        help_text=_('Above Ground Sighter Measurement'),
+        max_length = 10,
+        null=True,
+        blank=True,
+    )
+    Specific_Location = models.CharField(
+        help_text=_('Specific Location'),
+        max_length=100,
+        null=True,
+        blank=True,
+    )
+
+    Running = models.CharField(
+        help_text=_('Running'),
+        choices=((TRUE,'true'),(FALSE,'false')),default=FALSE,max_length=5,null=True)
+
+    Chasing = models.CharField(
+        help_text=_('Chasing'),
+        choices=((TRUE,'true'),(FALSE,'false')),default=FALSE,max_length=5,null=True)
+
+    Climbing = models.CharField(
+        help_text=_('Climbing'),
+        choices=((TRUE,'true'),(FALSE,'false')),default=FALSE,max_length=5,null=True)
+
+    Eating = models.CharField(
+        help_text=_('Eating'),
+        choices=((TRUE,'true'),(FALSE,'false')),default=FALSE,max_length=5,null=True)
+
+    Foraging = models.CharField(
+        help_text=_('Foraging'),
+        choices=((TRUE,'true'),(FALSE,'false')),default=FALSE,max_length=5,null=True)
+
+    Other_Activities = models.CharField(
+        help_text=_('Other Activities'),
+        max_length=100,
+        null=True,
+        blank=True,
+    )
+
+    Kuks = models.CharField(
+        help_text=_('Kuks'),
+        choices=((TRUE,'true'),(FALSE,'false')),default=FALSE,max_length=5,null=True)
+
+    Quaas = models.CharField(
+        help_text=_('Quaas'),
+        choices=((TRUE,'true'),(FALSE,'false')),default=FALSE,max_length=5,null=True)
+
+    Moans = models.CharField(
+        help_text=_('Moans'),
+        choices=((TRUE,'true'),(FALSE,'false')),default=FALSE,max_length=5,null=True)
+
+    Tail_Flags = models.CharField(
+        help_text=_('Tail flags'),
+        choices=((TRUE,'true'),(FALSE,'false')),default=FALSE,max_length=5,null=True)
+
+    Tail_Twitches = models.CharField(
+        help_text=_('Tail twitches'),
+        choices=((TRUE,'true'),(FALSE,'false')),default=FALSE,max_length=5,null=True)
+
+    Approaches = models.CharField(help_text=_('Approaches'),
+            choices=((TRUE,'true'),(FALSE,'false')),default=FALSE,max_length=5,null=True)
+    Indifferent = models.CharField(help_text=_('Indifferent'),
+            choices=((TRUE,'true'),(FALSE,'false')),default=FALSE,max_length=5,null=True)
+    Runs_From = models.CharField(help_text=_('Runs from'),
+            choices=((TRUE,'true'),(FALSE,'false')),default=FALSE,max_length=5,null=True)
+    
+    Other_Interactions = models.CharField(
+        help_text=_('Other Interactions'),
+        max_length=100,
+        null=True,
+        blank=True,
+    )
 
