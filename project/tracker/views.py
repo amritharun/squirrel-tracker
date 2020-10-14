f83f32c2243504c3130e8e3d7cd9180d18f20c93
cg3215 <cg3215@columbia.edu>
2020-10-14 15:03:00 -0400

    Added main and get_stats in views.py

--- project/tracker/views.py
+++ project/tracker/views.py
@@ -1,7 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse 
 
-def index(request):
-    return HttpResponse('Hello')
+from .models import Sighting
+
+
+def main(request):
+    return render(request, 'tracker/main.html')
+
+def get_stats(request):
+    AM = 0
+    PM = 0
+    G = 0
+    C = 0
+    B = 0
+    for m in Sighting.objects.all():
+        if m.SHIFT == 'AM':
+            AM += 1
+        if m.SHIFT == 'PM':
+            PM += 1
+        if m.COLOR == 'Grey':
+            G += 1
+        if m.COLOR == 'Cinnamon':
+            C += 1
+        if m.COLOR == 'Black':
+            B += 1
+    context = {
+	'AM':AM,
+	'PM':PM,
+	'G':G,
+	'C':C,
+	'B':B,
+	}
+    Return render(request, 'tracker/stats.html', context)
 
 # Create your views here.
